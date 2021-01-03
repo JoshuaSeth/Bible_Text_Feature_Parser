3009dcca4e0253e79e00958e5c6b95c968758b0a
JoshuaSeth <SB@accette.eu>
2021-01-03 17:13:20 +0100

    rm workspace bug

--- app/workspace.py
+++ app/workspace.py
@@ -1,8 +1,8 @@
 class WorkSpace:
     def __init__(self):
-        self.__dict__["Passages"] = None
-        self.__dict__["Data"] = None
-        self.__dict__["Plugins"] = None
+        self.__dict__["Passages"] = []
+        self.__dict__["Data"] = []
+        self.__dict__["Plugins"] = []
     
 
 
